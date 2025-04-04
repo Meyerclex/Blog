507ec54f58e6445e596acca784da539b45eb47cd
Meyer <94032239+Meyerclex@users.noreply.github.com>
2025-04-04 18:22:34 +0800

    1

--- themes/newspaper/assets/js/mouse_coords.js
+++ themes/newspaper/assets/js/mouse_coords.js
@@ -5,6 +5,6 @@ p.innerHTML = "C'mon, move your mouse!"
 document.body.append(p);
 
 document.addEventListener("mousemove", e => {
-  p.innerHTML = `mouseX: ${e.clientX}, mouseY: ${e.clientY}`;  
+  p.innerHTML = `mouseX: ${e.clientX}, mouseY: ${e.clientY}`;
 });
 
